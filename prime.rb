85f6afb383eecc8cc12420b23f6d2648af262e0e
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-06 18:43:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(num)
+ if num.negative? || num < 2 || num.is_a?(Float)
+    false
+ elsif num == 2 || 3
+    true
+ else num >= 4
+   n = 2
+   while n != num
+ if num % n == 0
+  return  false
+  else n += 1
+    num == n ? true : false
+   end
+  end
+ end
+end
